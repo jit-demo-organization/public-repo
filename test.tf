5e8e822222ccd15e0e234fbd4a44e818207b8e1b
JitDemo <128039000+JitDemo@users.noreply.github.com>
2023-03-21 08:29:05 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,18 +1,14 @@
-resource "aws_s3_bucket_object" "data_objefct" {
-  bucket = aws_s3_bucket.data_bucket.id
-  key    = "data.csv"
+resource "aws_redshift_cluster" "example1" {
+  cluster_identifier = "example"
+  publicly_accessible = true
 }
 
-resource "aws_s3_bucket_object" "data_object2" {
-  bucket = aws_s3_bucket.data_bucket.id
-  key    = "data.csv"
-  server_side_encryption = ""
+resource "aws_redshift_cluster" "example2" {
+  cluster_identifier = "example"
+  publicly_accessible = null
 }
 
-
-resource "aws_s3_bucket_object" "data_object33" {
-  bucket = aws_s3_bucket.data_bucket.id
-  key    = "data.csv"
-  server_side_encryption = "AES256"
-  server_side_kms_key_id = "arn:aws:kms:us-east-1:000000000000:key/00000000-0000-0000-0000-000000000000"
+resource "aws_redshift_cluster" "example3" {
+  cluster_identifier = "example"
+  publicly_accessible = false
 }
